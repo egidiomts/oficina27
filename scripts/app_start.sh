4331e48d6bed1a2180ce11de4ab0aa3d9ef84044
raphael <r.egidio.matos@gmail.com>
2025-09-09 17:20:41 -0400

    coimosim

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Definir HOME para PM2
-export HOME=/home/ec2-user 
-
 # Vai para o diretório do backend
 cd /var/www/html/oficina27/backend
 
 # Inicia pelo PM2
-pm2 start --name oficina27 "sudo npm start" --update-env
-
-# Salva processo PM2
-pm2 save
+pm2 start --name oficina27 "npm start" --update-env
