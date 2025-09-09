256ebc3dba6eca5f7a40adb25b13787c798e420f
raphael <r.egidio.matos@gmail.com>
2025-09-09 17:13:09 -0400

    commit13

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,16 +2,13 @@
 set -euo pipefail
 
 # Definir HOME para PM2
-export HOME=/home/ec2-user
-
-# Deleta a instância antiga, se existir
-pm2 delete oficina27 || true 
+export HOME=/home/ec2-user 
 
 # Vai para o diretório do backend
 cd /var/www/html/oficina27/backend
 
 # Inicia pelo PM2
-pm2 start --name oficina27 "npm start" --update-env
+pm2 start --name oficina27 "sudo npm start" --update-env
 
 # Salva processo PM2
 pm2 save
