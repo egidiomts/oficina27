d8e727c040056166f32794d32eaa8b0050465d58
raphael <r.egidio.matos@gmail.com>
2025-09-09 17:08:37 -0400

    commit 12

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export HOME=/home/ec2-user
 
 # Deleta a instância antiga, se existir
-pm2 delete oficina27 
+pm2 delete oficina27 || true 
 
 # Vai para o diretório do backend
 cd /var/www/html/oficina27/backend
