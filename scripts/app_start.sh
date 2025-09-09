66959636c53a5130921a42c842cc9d21d794b748
raphael <r.egidio.matos@gmail.com>
2025-09-09 16:56:02 -0400

    commit10

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
-sudo pm2 delete oficina27 || true
 
+# Definir HOME para PM2
+export HOME=/home/ec2-user
+
+# Deleta a instância antiga, se existir
+pm2 delete oficina27 || true
+
+# Vai para o diretório do backend
 cd /var/www/html/oficina27/backend
 
 # Inicia pelo PM2
-sudo pm2 start server.js --name oficina27 --update-env
-sudo pm2 save
+pm2 start server.js --name oficina27 --update-env
+
+# Salva processo PM2
+pm2 save
